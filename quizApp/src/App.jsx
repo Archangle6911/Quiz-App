fffb13a6db83634ed30f92b30ef443daef35bb57
Ali Munir Abdullahi <alimunirabdullahi@gmail.com>
2025-12-20 09:16:25 +0300

    Update App.jsx

--- quizApp/src/App.jsx
+++ quizApp/src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./components/Home";
 import Quiz from "./components/Quiz";
 import Result from "./components/Result";
+import About from "./components/About";
+import Login from "/./components/Login";
 import "./style.css";
 
 export default function App() {
@@ -10,6 +12,8 @@ export default function App() {
       <Route path="/" element={<Home />} />
       <Route path="/quiz" element={<Quiz />} />
       <Route path="/result" element={<Result />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/login" element={<Login />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
